d19e2bef2a061b7fdb4eb986e33111abf1e46004
Jeremy Gigase <jeremy.gigase@gmail.com>
2021-02-16 12:23:21 +0100

    horizontal carousel with pagination, added Pagination Component & package image-picker

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, memo, useRef, useCallback, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -26,41 +26,97 @@ export default function App() {
   );
 }
 
-function Slide({ data }) {
-  const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
+const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
+
+const Slide = memo(function Slide({ data }) {
   return (
-    <View
-      style={{
-        height: windowHeight,
-        width: windowWidth,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Image
-        source={{ uri: data.image }}
-        style={{
-          width: windowWidth * 0.9,
-          height: windowHeight * 0.9,
-        }}></Image>
-      <Text style={{ fontSize: 24 }}>{data.title}</Text>
-      <Text style={{ fontSize: 18 }}>{data.subtitle}</Text>
+    <View style={styles.slide}>
+      <Image source={{ uri: data.image }} style={styles.slideImage}></Image>
+      <Text style={styles.slideTitle}>{data.title}</Text>
+      <Text style={styles.slideSubtitle}>{data.subtitle}</Text>
+    </View>
+  );
+});
+
+function Pagination({ index, slideList }) {
+  return (
+    <View style={styles.pagination} pointerEvents='none'>
+      {slideList.map((_, i) => {
+        return (
+          <View
+            key={i}
+            style={[
+              styles.paginationDot,
+              index === i
+                ? styles.paginationDotActive
+                : styles.paginationDotInactive,
+            ]}
+          />
+        );
+      })}
     </View>
   );
 }
 
 function Carousel({ slideList }) {
-  console.log(slideList);
+  const [index, setIndex] = useState(0);
+  const indexRef = useRef(index);
+  indexRef.current = index;
+  const onScroll = useCallback((event) => {
+    const slideSize = event.nativeEvent.layoutMeasurement.width;
+    const index = event.nativeEvent.contentOffset.x / slideSize;
+    const roundIndex = Math.round(index);
+
+    const distance = Math.abs(roundIndex - index);
+
+    // Prevent one pixel triggering setIndex in the middle
+    // of the transition. With this we have to scroll a bit
+    // more to trigger the index change.
+    const isNoMansLand = 0.4 < distance;
+
+    if (roundIndex !== indexRef.current && !isNoMansLand) {
+      setIndex(roundIndex);
+    }
+  }, []);
+
+  // Use the index
+  // useEffect(() => {
+  //   console.warn(index);
+  // }, [index]);
+
+  const flatListOptimizationProps = {
+    initialNumToRender: 0,
+    maxToRenderPerBatch: 1,
+    removeClippedSubviews: true,
+    scrollEventThrottle: 16,
+    windowSize: 2,
+    keyExtractor: useCallback((e) => e.id, []),
+    getItemLayout: useCallback(
+      (_, index) => ({
+        index,
+        length: windowWidth,
+        offset: index * windowWidth,
+      }),
+      []
+    ),
+  };
+
   return (
-    <FlatList
-      data={slideList}
-      style={{ flex: 1 }}
-      renderItem={({ item }) => {
-        return <Slide data={item} />;
-      }}
-      // pagingEnabled
-      // horizontal
-      // showsHorizontalScrollIndicator={false}
-    />
+    <>
+      <FlatList
+        data={slideList}
+        style={styles.carousel}
+        renderItem={({ item }) => {
+          return <Slide data={item} />;
+        }}
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onScroll={onScroll}
+        {...flatListOptimizationProps}
+      />
+      <Pagination index={index} slideList={slideList}></Pagination>
+    </>
   );
 }
 
@@ -71,4 +127,31 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  slide: {
+    height: windowHeight,
+    width: windowWidth,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  slideImage: { width: windowWidth * 0.8, height: windowHeight * 0.8 },
+  slideTitle: { fontSize: 24 },
+  slideSubtitle: { fontSize: 18 },
+
+  pagination: {
+    position: "absolute",
+    bottom: 40,
+    width: "100%",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 2,
+  },
+  paginationDotActive: { backgroundColor: "lightblue" },
+  paginationDotInactive: { backgroundColor: "gray" },
+
+  carousel: { flex: 1 },
 });
